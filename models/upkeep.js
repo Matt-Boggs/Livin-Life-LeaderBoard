912d9b24caa43e13d32ce044a7841dcdd3a3385b
Matt Boggs <matt.david.boggs@gmail.com>
2021-01-03 17:42:31 -0700

    preliminary chore model

---
+++ models/upkeep.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const upkeepSchema = new Schema({
+
+    // MOST OF THESE THINGS WILL BE ARRAYS FOR NOW, THE USER SHOULD BE PICKING FROM CHOICES, NOT PROVIDING THEIR OWN
+
+
+  name: { type: String, required: true },
+  // Everything below is preliminary
+
+  // something like [Physical, Mental, Spiritual]
+  // FOR EXAMPLE, but this isnt likely to make the cut
+  category: {type: Array, required: true},
+
+  // This would maybe be [5, 15, 30, 60] in minutes
+  timeCost: {type: Array, required: true},
+
+  // For lack of a better word, perennial meaning, how often this chore should be done, ie daily, weekly, monthly, etc...
+  perennial: {type: Array, required: true}
+
+});
+
+const Champion = mongoose.model("Champion", championSchema);
+
+module.exports = Champion;
